0d7b31fce803aaf137895699d6aeae719a59871c
Yerson León <yelemo777@gmail.com>
2021-09-22 20:03:47 -0500

    Agregando al comit todos los cambios

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 const express = require('express');
-express();
+
+const config = require('./server/config');
+
+//database
+require('./database');
+
+//Empezando el servidor
+const app = config(express());
+
+
+app.listen(app.get('port'),()=>{
+    console.log('Server on port', app.get('port'));
+});
